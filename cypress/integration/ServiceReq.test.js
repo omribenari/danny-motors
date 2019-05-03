8b3c3b5d142706f4276364cc12886645b2aaaf78
obenari <Omri_BenAri@intuit.com>
2019-05-03 17:12:47 +0300

    SAVEPOINT

--- cypress/integration/ServiceReq.test.js
+++ cypress/integration/ServiceReq.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 describe('Service request page', () => {
-
   beforeEach(() => {
     cy.visit('service-req');
   });
@@ -20,5 +19,27 @@ describe('Service request page', () => {
     cy.get('#note');
     cy.get('span[data-cy="checkbox-accept"]');
     cy.get('button[type="submit"]');
+  });
+
+  it.only('loads successfully', () => {
+    cy.get('#full-name').type('omri ben ari');
+    cy.get('#email').type('omri79@gmail.com');
+    cy.get('#phone').type('123123123123');
+    cy.get('#select-selectedCarMake')
+      .click()
+      .then(() => {
+        cy.get('li[data-value="Jaguar"]').click();
+      });
+    cy.get('#select-selectedModel')
+      .click()
+      .then(() => {
+        cy.get('li[data-value="XJ"]').click();
+      });
+    cy.get('#car-year').type('2008');
+    cy.get('#car-km').type('100000');
+    cy.get('#car-lp').type('23-567-00');
+    cy.get('#note').type('khsf kfhskjdfh ksjdhfkjshdfkjshfkjsdhf sdkjfh  skjdfh skdjfhks dfksj hfkjshfkjsdhf dsk');
+    cy.get('span[data-cy="checkbox-accept"]').click();
+    cy.get('button[type="submit"]').click();
   });
 });
